997ab561fc369d11180fd1b050457cbe21e08dce
Jim King <kingjim@smartekworks.com>
2014-08-28 20:47:42 +0800

    update

--- test/app.js
+++ test/app.js
@@ -3,6 +3,10 @@
  * @class App
  * @constructor
  */
+
+if (!window.console) window.console = {};
+if (!window.console.log) window.console.log = function () { };
+
 function App() {
 	var self = this;
 
